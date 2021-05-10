ba540324f930e4322c65fbe447e29aeaf4e63002
Jonathan Herrera <jhbioquimico@gmail.com>
2021-05-09 20:37:22 -0400

    Pagina Viajes Chile Completa
    Viajes Chile completa

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("a").click(function(event){
+        event.preventDefault()
+
+        var gato = this.hash;
+
+        $("html").animate(
+        {
+            scrollTop: $(gato).offset().top -20,
+        }, 800
+        )
+    })
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
